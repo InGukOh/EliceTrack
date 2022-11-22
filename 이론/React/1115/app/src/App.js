e834fe42d4dbff55b2ab8762e4fa1ee225137392
InGukOh <87802273+InGukOh@users.noreply.github.com>
2022-11-22 10:22:16 +0900

    1118 이론 commit

--- 이론/React/1115/app/src/App.js
+++ 이론/React/1115/app/src/App.js
@@ -1,48 +1,37 @@
-import "./App.css";
+import './App.css';
+import ReactPlayer from 'react-player'
 
-function Header() {
-  return (
-    <header>
-      <h1>
-        <a href="index.html">WEB</a>
-      </h1>
-    </header>
-  );
+const Header = ({title})=>{
+  return <header>
+    <h1><a href="index.html">{title}</a></h1>
+  </header>
 }
-
-function Nav() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <a href=" 1.html">html</a>
-        </li>
-        <li>
-          <a href=" 2.css">css</a>
-        </li>
-        <li>
-          <a href=" 3.js">js</a>
-        </li>
-      </ul>
-    </nav>
-  );
+const Nav = ({topics})=>{
+  const liTag = topics.map((t)=><li key={t.id}><a href={"/read/"+t.id}>{t.title}</a></li>);
+  return <nav>
+    <ul>
+        {liTag}
+    </ul>
+  </nav>
 }
-
-function Article() {
-  return (
-    <article>
-      <h2>WELCOM</h2>
-      Hello, WEB
-    </article>
-  );
+const Article = ({title, body})=>{
+  return <article>
+    <h2>{title}</h2>
+    {body}
+  </article>
 }
-
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'js', body:'js is ...'}
+  ]
   return (
     <div className="App">
-      <Header />
-      <Nav />
-      <Article />
+      <Header title="웹" />
+      <Nav topics={topics} />
+      <Article title="어서오세요!" body="웹의 세계로 초대합니다."/>
+      {/* <ReactPlayer url='https://www.youtube.com/watch?v=7fy1eFEkrpU' width={640} /> */}
     </div>
   );
 }
